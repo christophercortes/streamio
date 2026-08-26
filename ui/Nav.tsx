701ad0a256af71bc7daf8e33f0c9ec2517b16a48
christophercortes <christopherctes@hotmail.com>
2026-08-25 21:29:48 -0400

    logo

--- ui/Nav.tsx
+++ ui/Nav.tsx
@@ -13,10 +13,10 @@ export default function Nav() {
                     className="ml-15"
                     alt="Logo"
                 />
-                <p className="mt-2 ml-5 text-zinc-400">Watch live TV anywhere</p>
             </div>
-            <div className="mt-5 text-4xl font-bold">
-                <p>Streamio</p>
+            <div className="ml-5 mt-5">
+                <p className="text-4xl font-bold">Streamio</p>
+                <p className="text-zinc-400">Watch live TV anywhere</p>
             </div>
             <div className="mt-5 text-4xl font-bold text-yellow-400">
                 <p>+</p>
