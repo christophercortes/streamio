0008ef2fdb0251661bf477321040a7c92f5ce4b3
christophercortes <christopherctes@hotmail.com>
2026-08-26 19:09:54 -0400

    navigation

--- ui/Nav.tsx
+++ ui/Nav.tsx
@@ -2,24 +2,24 @@
 
 import Image from "next/image";
 
-export default function Nav() {
+export default function Navigation() {
     return (
-        <div className="flex px-10 py-10 bg-zinc-950 text-white dark:bg-black">
+        <div className="flex items-center px-4 py-5 sm:px-6 md:px-10 md:py-8 bg-zinc-950 text-white dark:bg-black">
             <div>
-                <Image
+                {/* <Image
                     src="/logo.png"
-                    width={100}
-                    height={80}
+                    width={70}
+                    height={56}
                     className="ml-15"
                     alt="Logo"
-                />
+                /> */}
             </div>
-            <div className="ml-5 mt-5">
-                <p className="text-4xl font-bold">Streamio</p>
+            <div className="m-1">
+                <p className="text-4xl font-bold">Streamio<span className="text-yellow-400">+</span></p>
                 <p className="text-zinc-400">Watch live TV anywhere</p>
             </div>
-            <div className="mt-5 text-4xl font-bold text-yellow-400">
-                <p>+</p>
+            <div className="ml-auto border-2 border-solid px-2 font-semibold">
+                <button>Log In</button>
             </div>
         </div>
     )
