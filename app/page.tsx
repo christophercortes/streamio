c655b09784bb77fce583c3baff4b5fc213fa5c28
christophercortes <christopherctes@hotmail.com>
2026-08-26 20:46:06 -0400

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { channels } from "@/lib/channels";
+import ChannelCarousel from "@/ui/ChannelCarousel";
 
 export default function Home() {
+  const [selectedChannelId, setSelectedChannelId] = useState(channels[0].id);
+
+  const selectedChannel = channels.find(
+    (channel) => channel.id === selectedChannelId
+  )!;
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-4xl font-semibold">Welcome to Streamio+</h1>
+    <div className="flex flex-col flex-1 items-center justify-center text-white bg-zinc-800 font-sans dark:bg-black">
+      <main className="flex flex-1 flex-col py-4 items-center dark:bg-black">
+        <h1 className="text-xl font-semibold">Watch live TV from top channels</h1>
+        <ChannelCarousel
+          selectedChanneld={selectedChannelId}
+          onSelectedChannel={setSelectedChannelId}
+        />
+        <button className="mt-10 text-blue-400">See all channels</button>
       </main>
       <footer>
           <p>©{ new Date().getFullYear()} All rights reserved.</p>
