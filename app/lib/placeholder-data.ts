549be8034fbd3675ca870b599532523d8e1e9925
christophercortes <christopherctes@hotmail.com>
2026-09-01 17:54:07 -0400

    routes fixed

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -2,7 +2,7 @@ const user = [
     {
         id: '46944cf9-8586-4a41-8077-786ebdf690d8',
         name: 'Christopher',
-        email: 'chris@mail.com',
+        email: 'christopher.cortes.eduardo@gmail.com',
         password: 'chris@',
     }
 ];
