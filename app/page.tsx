fc6376e028de403e065cec93dba73c5fc1934e9c
christophercortes <christopherctes@hotmail.com>
2026-09-03 20:13:58 -0400

    home page small screen content

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,46 @@
 "use client";
 
 import ChannelCarousel from "@/app/ui/ChannelCarousel";
+import Cinematic from "./ui/Cinematic";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-zinc-900 text-white dark:bg-black">
       <main className="w-full flex-1">
-        <div className="mx-auto flex w-full max-w-7xl flex-col px-8 py-6 sm:px-26 sm:py-8 lg:px-30 lg:py-10">
-          <div className="mb-6">
-            <h1 className="text-center text-xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
+
+        <Cinematic
+          video="/pampa.mp4"
+          category="Teleserie"
+          title="Pampa Ilusion"
+          description="from the 90's"
+        />
+        <div className="mx-auto w-full max-w-7xl flex-col">
+          <div className="mb-8 text-center">
+            <h1 className="text-3xl px-2 font-bold tracking-tight sm:text-4xl md:text-5xl">
               Watch live TV from top channels
             </h1>
-            <p className="mt-2 text-center text-sm font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
+            <p className="mx-auto mt-3 max-w-2xl text-sm leading-6 text-zinc-400 sm:base-2xl md:text-lg">
               Choose a channel and start watching live
             </p>
           </div>
-          <ChannelCarousel/>
+
+          <ChannelCarousel />
           <div className="mt-6 flex justify-center sm:mt-8">
             <button
               type="button"
-              className="rounded-lg px-4 py-2 text-sm font-medium text-blue-400 transition hover:bg-zinc-800 hover:text-blue-300 sm:text-base">
+              className="group inline-flex items-center gap-2 rounded-lg px-3 py-4 text-sm font-semibold text-zinc-300 transition hover:bg-white/5 hover:text-white sm:text-base">
               See all channels
+              <span className="text-yellow-400 transition-transform duration-200 group-hover:translate-x-1"
+              >
+                →
+              </span>
             </button>
           </div>
         </div>
       </main>
-      <footer className="border-t border-zinc-800 px-4 py-5 text-center text-xs text-zinc-500 sm:py-6 sm:text-sm">
-          <p>©{ new Date().getFullYear()} Streamio+. All rights reserved.</p>
-        </footer>
+      <footer className="border-t border-white/40 px-4 py-5 text-center text-xs text-zinc-500 sm:py-6 sm:text-sm">
+        <p>©{new Date().getFullYear()} <span className="text-white">Streamio+</span>. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
