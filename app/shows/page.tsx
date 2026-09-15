da2cfd3ba176393906a27c522f21a036118e5ade
christophercortes <christopherctes@hotmail.com>
2026-09-15 18:46:06 -0300

    shows page

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,20 +1,27 @@
+import ShowSection from "../ui/ShowSection"
+import { shows } from "../lib/shows"
+
 export default function Shows() {
     return (
-        <main className="flex min-h-[cal(100vh-64px)] items-center justify-center bg-black px-6">
-            <section className="text-center">
-                <div className="m-6 text-6xl">
-                    📺
-                </div>
-                <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-                    Shows
-                </h1>
-                <p className="mx-auto mt-4 max-w-md text-base leading-relaxed text-zinc-400 sm:text-lg">
-                    We're working on something exciting.
-                    <br />
-                    Movies will be available here soon.
-                </p>
-                <div className="mx-auto mt-8 h-1 w-16 rounded-full bg-white" />
-            </section>
+        <main className="">
+                <section>
+                    <ShowSection
+                        title="Latin America"
+                        shows={shows.filter(
+                            (show) => show.country === "Latin America"
+                        )}
+                    />
+                </section>
+                <div className="border-t border-zinc-800" />
+                {/* <section>
+                    <ShowSection
+                        title="International Shows"
+                        shows={shows.filter(
+                            (show) => show.language === "English"
+                        )}
+                    />
+                </section>
+                <div className="border-t border-zinc-800" /> */}
         </main>
     )
 }
--- app/ui/ShowSection.tsx
+++ app/ui/ShowSection.tsx
@@ -10,13 +10,14 @@ export default function ShowSection({
     title, shows
 }: ShowSectionProps) {
     return (
-        <section>
-            <h2>
+        <section className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+            <h2 className="m-6 text-2xl font-bold tracking-tight sm:text-3xl">
                 {title}
             </h2>
-            <div>
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
                 {shows.map((show) => (
                     <ShowCard
+                        key={show.id}
                         id={show.id}
                         name={show.name}
                         logo={show.logo}
